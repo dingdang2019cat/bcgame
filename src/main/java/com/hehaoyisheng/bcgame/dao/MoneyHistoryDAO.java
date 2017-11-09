124837c0a65a373c0857ecc79cd7af722609f4b0
lnzyz <lnzyz@126.com>
2017-11-10 01:46:43 +0800

    dao

--- src/main/java/com/hehaoyisheng/bcgame/dao/MoneyHistoryDAO.java
+++ src/main/java/com/hehaoyisheng/bcgame/dao/MoneyHistoryDAO.java
@@ -1,4 +1,12 @@
 package com.hehaoyisheng.bcgame.dao;
 
+import com.hehaoyisheng.bcgame.entity.MoneyHistory;
+
+import java.util.List;
+
 public interface MoneyHistoryDAO {
+
+    int insert(MoneyHistory moneyHistory);
+
+    List<MoneyHistory> select(MoneyHistory moneyHistory);
 }
