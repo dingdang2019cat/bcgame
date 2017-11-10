1a65087bf047e439eef26db41f5b73913da373d1
lnzyz <lnzyz@126.com>
2017-11-10 23:27:40 +0800

    dao

--- src/main/java/com/hehaoyisheng/bcgame/manager/ProxyManager.java
+++ src/main/java/com/hehaoyisheng/bcgame/manager/ProxyManager.java
@@ -1,4 +1,13 @@
 package com.hehaoyisheng.bcgame.manager;
 
+import com.hehaoyisheng.bcgame.entity.Proxy;
+
+import java.util.List;
+
 public interface ProxyManager {
+    int insert(Proxy user);
+
+    int delete(int id);
+
+    List<Proxy> select(Proxy user);
 }
