70db2800b9abe016ee51b34c912e872009fc8a6f
lnzyz <lnzyz@126.com>
2017-12-27 00:04:53 +0800

    报表中心接入

--- src/main/java/com/hehaoyisheng/bcgame/controller/IndexController.java
+++ src/main/java/com/hehaoyisheng/bcgame/controller/IndexController.java
@@ -2,19 +2,19 @@ package com.hehaoyisheng.bcgame.controller;
 
 import com.google.common.collect.Lists;
 import com.hehaoyisheng.bcgame.common.GameData;
+import com.hehaoyisheng.bcgame.entity.User;
 import com.hehaoyisheng.bcgame.entity.vo.LotteryTime;
 import com.hehaoyisheng.bcgame.entity.vo.Result;
 import com.hehaoyisheng.bcgame.manager.UserManager;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Controller
-@RequestMapping
+@SessionAttributes("user")
 public class IndexController {
 
     @RequestMapping(value = "/login", method = {RequestMethod.GET})
@@ -34,5 +34,21 @@ public class IndexController {
         list.add(new LotteryTime("pk10", GameData.gameTime.get("pk10")));
         list.add(new LotteryTime("3d", GameData.gameTime.get("3d")));
         return Result.success(list);
+    }
+
+    @RequestMapping("/lotts/{gameType}/index")
+    //TODO
+    public String gamePage(@ModelAttribute("user") User user, @PathVariable String gameType){
+        //开奖图片
+        //最近中奖
+        //最近投注
+        //
+        return null;
+    }
+
+    @RequestMapping("/lotts/{gameType}/info")
+    @ResponseBody
+    public Result info(@ModelAttribute("user") User user, @PathVariable String gameType){
+        return Result.success(null);
     }
 }
--- src/main/java/com/hehaoyisheng/bcgame/controller/LotteryController.java
+++ src/main/java/com/hehaoyisheng/bcgame/controller/LotteryController.java
@@ -123,5 +123,4 @@ public class LotteryController {
         return Result.success(resultList);
     }
 
-    
 }
