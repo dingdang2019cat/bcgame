0869e0b552e625d531fb05d0ed3b3599b3f9dac5
lnzyz <lnzyz@126.com>
2018-01-29 08:55:19 +0800

    报表中心接入

--- src/main/java/com/hehaoyisheng/bcgame/utils/test.java
+++ src/main/java/com/hehaoyisheng/bcgame/utils/test.java
@@ -17,32 +17,17 @@ import java.io.IOException;
 
 public class test {
     public static void main(String[] args) throws IOException {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         //LotteryThread lotteryThread = (LotteryThread) applicationContext.getBean("lotteryThread");
         //lotteryThread.lottery("cqssc", "20180121024", "0,0,0,1,0");
-        BcLotteryOddsDAO bcLotteryOddsDAO = (BcLotteryOddsDAO) applicationContext.getBean("bcLotteryOddsDAO");
+        //BcLotteryOddsDAO bcLotteryOddsDAO = (BcLotteryOddsDAO) applicationContext.getBean("bcLotteryOddsDAO");
         Document document = Jsoup.parse(new File("C:\\Users\\Administrator\\IdeaProjects\\bcgame\\src\\main\\resources\\ssc.jsp"), "utf-8");
         Elements elements = document.getElementsByClass("lottTypeDetail");
         for(Element element : elements){
             String dataShow = element.attr("data-show").replace("#", "");
-            String max = element.attr("data-maxbonus");
-            if(max.contains("-")){
-                continue;
-            }
-            BcLotteryOdds bcLotteryOdds = new BcLotteryOdds();
-            bcLotteryOdds.setLotteryType("ssc");
-            bcLotteryOdds.setBounsType(0);
-            bcLotteryOdds.setPlayType(dataShow);
-            bcLotteryOdds.setOdds(Double.valueOf(max));
-            bcLotteryOddsDAO.insert(bcLotteryOdds);
-            String bounds = element.attr("data-bonus");
-            BcLotteryOdds bcLotteryOdds1 = new BcLotteryOdds();
-            bcLotteryOdds1.setLotteryType("ssc");
-            bcLotteryOdds1.setBounsType(1);
-            bcLotteryOdds1.setPlayType(dataShow);
-            bcLotteryOdds1.setOdds(Double.valueOf(bounds));
-            bcLotteryOddsDAO.insert(bcLotteryOdds1);
-            System.out.println(dataShow + "   " + max + "   " + bounds);
+            String dataTitle = element.attr("data-title");
+            System.out.println("playName.put(\"" + dataShow + "\", \"" + dataTitle + "\");");
+
         }
         System.out.println(elements.size());
     }
